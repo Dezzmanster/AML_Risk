dba9c349f09b8aa6a4f24fb36117e361891227e9
ankostas <57225854+ankostas@users.noreply.github.com>
2020-10-22 09:49:10 +0300

    Add files via upload

--- preprocessor/fencoding.py
+++ preprocessor/fencoding.py
@@ -96,5 +96,127 @@ class Dtimetodata(object):
         if self.path != None:
               X.to_csv(self.path)
         return X
+    
+class FEncoding(object):
+    def __init__(self, n_jobs = 1, chunks = None, path = None):      
+        
+        self.categor_types = ['object', 'bool', 'int32', 'int64']
+        self.numer_types = ['float', 'float32', 'float64']
+        self.time_types = ['datetime64[ns]'] # What else?
+
+        if n_jobs == None:
+            self.n_jobs = 1
+        elif n_jobs == -1:
+            self.n_jobs = mp.cpu_count()
+        else:
+            self.n_jobs = n_jobs        
+        self.chunks = chunks
+        self.path = path  
+
+    def initialize_types_(self, X):
+        # Sometimes categorical feature can be presented with a float type.
+        # Sometimes numerical feature can be presented with an int type.  
+        # Let's check for that  
+        for column in X.columns:
+            c_type = str(X[column].dtype) 
+            unique_values_X = X[column].unique()        
+            if any(c_type == t for t in self.numer_types) & (len(unique_values_X) < 20):
+                print('\n {} has type {} and number of unique values: {}, will be considered as a categorical \n'.format(column, c_type, len(unique_values_X)))
+                self.categor_columns.append(column)            
+            elif any(c_type == t for t in self.categor_types) & (len(unique_values_X) > 20):
+                print('\n {} has type {} and number of unique values: {}, will be considered as a numerical \n'.format(column, c_type, len(unique_values_X)))
+                self.numer_columns.append(column)
+            elif any(c_type == t for t in self.categor_types):
+                self.categor_columns.append(column)
+            elif any(c_type == t for t in self.time_types):
+                self.time_columns.append(column)            
+            else:
+                self.numer_columns.append(column)
+        return self.categor_columns, self.numer_columns, self.time_columns
+    
+    def bucket_numerical_(self, X):
+        # TODO: specify or introduce a criterion which columns to bake
+        # K-bins discretization based on quantization
+
+        def get_input_keypoints(f_data, n_kps):
+            while len(np.quantile(f_data, np.linspace(0.0, 1.0, num=n_kps))) != len(np.unique(np.quantile(f_data, np.linspace(0.0, 1.0, num=n_kps)))):
+                n_kps -= 1
+            return np.quantile(f_data, np.linspace(0.0, 1.0, num=n_kps))
+
+        if self.columns_to_buck == 'all_numerical':
+            self.columns_to_buck = self.numer_columns            
+        if type(self.columns_to_buck) != list:
+            raise VlaueError('Identify list of columns_to_buck')
+
+        for column in X.columns:
+            if any(column == col for col in self.columns_to_buck):
+                print('\n {} bucketing ...'.format(column))
+                f_X = X[column].values.ravel()
+                X[column + '_bucketed'] = np.digitize(f_X, get_input_keypoints(f_X, self.n_bins))
+                if self.drop_current:
+                    X.drop(columns=[column], inplace=True)
+        return X
+
+    def encode_categor_(self, X, method = 'OrdinalEncoder'):
+        
+
+        if self.method == 'OrdinalEncoder':
+            enc = preprocessing.OrdinalEncoder()
+            X = pd.DataFrame(enc.fit_transform(X), columns = X.columns)
+
+        if self.method == 'OneHotEncoder':            
+            X = pd.get_dummies(X, drop_first=True, dummy_na=True)
+        
+        return X
+
+    
+    def initialize_types(self, X):
+        self.categor_columns, self.numer_columns, self.time_columns = [], [], []
+        if self.chunks == None:
+            self.chunks  = int(len(X.columns)/self.n_jobs)
+        return_ = Pool(processes = self.n_jobs).map(self.initialize_types_, 
+                                 [X[list(X.columns)[start: start + self.chunks]] for start in range(0, len(X.columns), self.chunks)]
+                                 )
+        
+        for i in range(len(return_)):
+            categor_columns, numer_columns, time_columns = return_[i]
+            self.categor_columns += categor_columns
+            self.numer_columns += numer_columns
+            self.time_columns += time_columns
+        return {'categor_columns': self.categor_columns,
+                           'numer_columns': self.numer_columns,
+                           'time_columns': self.time_columns       
+         }
+
+    def bucket_numerical(self, X, 
+                         n_bins=5, columns_to_buck = 'all_numerical', 
+                         drop_current = False):      
+        self.n_bins = n_bins
+        self.columns_to_buck = columns_to_buck
+        self.drop_current = drop_current
+        self.initialize_types(X)
+        if self.chunks == None:
+              self.chunks  = int(len(X.columns)/self.n_jobs)
+        X =  pd.concat(Pool(processes = self.n_jobs).map(self.bucket_numerical_, 
+                                [X[list(X.columns)[start: start + self.chunks]] for start in range(0, len(X.columns), self.chunks)]
+                                ), axis=1)
+        if self.path != None:
+              X.to_csv(self.path)
+        return X
+
+    def encode_categor(self, X, method = 'OrdinalEncoder'):
+        self.method = method
+        self.initialize_types(X)        
+        if self.chunks == None:
+            self.chunks  = int(len(X.columns)/self.n_jobs)
+        categor_columns = [i for i in self.categor_columns if i in list(X.columns)]
+        numer_columns = [i for i in self.numer_columns if i in list(X.columns)]
+        X_oh =  pd.concat(Pool(processes = self.n_jobs).map(self.encode_categor_, 
+                                [X[categor_columns[start: start + self.chunks]] for start in range(0, len(categor_columns), self.chunks)]
+                                ), axis=1)
+        X = X[numer_columns].append(X_oh)       
+        if self.path != None:
+              X.to_csv(self.path)
+        return X
 
 
